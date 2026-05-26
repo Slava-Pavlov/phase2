136a40336646e220cf3571d553f8c04ddf1f114f
Slava-Pavlov <spawlow87@gmail.com>
2026-05-26 17:24:04 +0700

    Elif done

--- hello.py
+++ hello.py
@@ -1,2 +1,7 @@
-x = int(input("What is your age now?"))
-print(f"Well, then, you were born in {2026-x}")
+x = int(input("How old are you?"))
+if x>=18 and x<99:
+	print("Good for you!")
+elif x>99:
+	print("Wow, you're an old-timer!")
+else:
+	print("That's negative")
