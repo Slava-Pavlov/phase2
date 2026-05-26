ca0d75284fcdab325d652502ed9d91a804877874
Slava-Pavlov <spawlow87@gmail.com>
2026-05-26 22:10:19 +0700

    Functions done

--- hello.py
+++ hello.py
@@ -1,8 +1,11 @@
-x=8
-while True:
-	y=int(input("Guess a number from 1 to 10: "))
-	if x!=y:
-		print("Guess again!")
-	else:
-		break
-print("Jackpot!")
+import random
+def guesser(x):
+	while True:
+		y=int(input("Guess a number from 1 to 10: "))
+		if x!=y:
+			print("Guess again!")
+		else:
+			break
+	print("Jackpot!")
+x = random.randint(1,10)
+guesser(x)
